497e6680f1ba97ce9a56aec2d4d8e658ba8d015d
Joao <joaocarlosguarnier@hotmail.com>
2022-01-11 16:18:58 -0300

    client model done

--- backend/src/main/java/com/devsuperior/dsclient/model/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/model/Client.java
@@ -1,4 +1,42 @@
 package com.devsuperior.dsclient.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String cpf;
+    private BigDecimal income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDate.now();
+    }
+
 }
