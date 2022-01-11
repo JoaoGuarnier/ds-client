90d1bda915caf2a518bbe722c443c278940e4b49
Joao <joaocarlosguarnier@hotmail.com>
2022-01-11 16:17:36 -0300

    client dto done

---
+++ backend/src/main/java/com/devsuperior/dsclient/dto/ClientDto.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dsclient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDto {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private BigDecimal income;
+    private LocalDate birthDate;
+    private Integer children;
+
+}
